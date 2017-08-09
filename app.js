653806fd227482b6a13727a0dea72d4f98ea5dde
topher <topher.wenceslaus@gmail.com>
2017-08-09 16:23:02 +0530

    edit

--- app.js
+++ app.js
@@ -25,7 +25,11 @@ server.get('/echo/weather', function (req, res, next) {
 
           return axios.get(`https://api.darksky.net/forecast/f254fa805ed7d9b3aeaa0cb19d976867/${lat + ',' + lng}`);
         }).then(function (response) {
-          res.send(`current temperature is ${response.data.currently.temperature} but feels like ${response.data.currently.apparentTemperature}`);
+          return res.send({
+            speech: `current temperature is ${response.data.currently.temperature} but feels like ${response.data.currently.apparentTemperature}`,
+            displayText: `current temperature is ${response.data.currently.temperature} but feels like ${response.data.currently.apparentTemperature}`,
+            source: 'webhook-echo-sample'
+          });
         })
         .catch(function (error) {
           console.log(error);
