6687759664b2a52d71b906dbbe0e34659a8e3f6f
topher <topher.wenceslaus@gmail.com>
2017-08-09 15:33:48 +0530

    edits

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var restify = require('restify');
 var axios = require('axios');
- 
- 
+
+
 const server = restify.createServer({
   name: 'myapp',
   version: '1.0.0'
@@ -9,24 +9,29 @@ const server = restify.createServer({
 server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
- 
- 
+
+
+server.get('/', function (req, res, next) {
+  res.send('welcome home');
+});
+
 server.get('/echo/weather', function (req, res, next) {
-    let location = 'Bangalore';
-      axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}`)
+  const location = 'Bangalore';
+  axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}`)
         .then(function (response) {
-          let lat = response.data.results[0].geometry.location.lat;
-          let lng = response.data.results[0].geometry.location.lng;
-          //secret key f254fa805ed7d9b3aeaa0cb19d976867
-          
-            return axios.get(`https://api.darksky.net/forecast/f254fa805ed7d9b3aeaa0cb19d976867/${lat+','+lng}`)
-        }).then(function(response){
-            res.send(`current temperature is ${response.data.currently.temperature} but feels like ${response.data.currently.apparentTemperature}`);        })
+          const lat = response.data.results[0].geometry.location.lat;
+          const lng = response.data.results[0].geometry.location.lng;
+          // secret key f254fa805ed7d9b3aeaa0cb19d976867
+
+          return axios.get(`https://api.darksky.net/forecast/f254fa805ed7d9b3aeaa0cb19d976867/${lat + ',' + lng}`);
+        }).then(function (response) {
+          res.send(`current temperature is ${response.data.currently.temperature} but feels like ${response.data.currently.apparentTemperature}`);
+        })
         .catch(function (error) {
           console.log(error);
         });
 });
- 
+
 server.listen(8080, function () {
   console.log('%s listening at %s', server.name, server.url);
 });
