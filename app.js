d316a5e4bc34392e0a443c43344ddb9b1e1b43af
topher <topher.wenceslaus@gmail.com>
2017-08-09 20:14:30 +0530

    edit

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ restService.get('/', function (req, res) {
 restService.post('/weather', function (req, res) {
   var location = req.body.result && req.body.result.parameters &&
   req.body.result.parameters.geocity ? req.body.result.parameters.geocity : 'Bangalore';
+  var willRain = req.body.result && req.body.result.parameters &&
+  req.body.result.parameters.willrain;
 
   axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}`)
         .then(function (response) {
@@ -25,13 +27,18 @@ restService.post('/weather', function (req, res) {
           const lng = response.data.results[0].geometry.location.lng;
           // secret key f254fa805ed7d9b3aeaa0cb19d976867
 
-          return axios.get(`https://api.darksky.net/forecast/f254fa805ed7d9b3aeaa0cb19d976867/${lat + ',' + lng}`);
+          return axios.get(`https://api.darksky.net/forecast/f254fa805ed7d9b3aeaa0cb19d976867/${lat + ',' + lng}?units=auto`);
         }).then(function (response) {
+          let speech = `summary:${response.data.daily.summary} 
+            CURRENT TEMP: ${response.data.currently.temperature};`;
+
+          if (willRain) {
+            speech = response.data.currently.icon === 'rain' ? 'Yes!! it will rain for sure. get your umberalla!!!!!' : 'no rain!!! no traffic njoi!!! kana kavo ghar javo';
+          }
+
           res.json({
-            speech: `summary:${response.data.daily.summary} 
-            CURRENT TEMP: ${response.data.currently.temperature} `,
-            displayText: `summary:${response.data.daily.summary} 
-            CURRENT TEMP: ${response.data.currently.temperature} `,
+            speech: speech,
+            displayText: speech,
             source: location
           });
         })
